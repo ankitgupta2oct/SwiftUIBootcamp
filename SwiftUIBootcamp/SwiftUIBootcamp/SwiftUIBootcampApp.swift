9249efd27d33fb637b1940200c8650ad42460821
Pooja Kamath <pooja.kamath@siemens.com>
2025-03-28 08:28:28 +0530

    Initial commit

---
+++ SwiftUIBootcamp/SwiftUIBootcamp/SwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampApp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Z003MK6T on 28/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
