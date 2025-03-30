f760ee2cf21eb34646d41a21e725c40e72400b9f
Pooja Kamath <pooja.kamath@siemens.com>
2025-03-30 17:58:35 +0530

    Gradient Screen

---
+++ SwiftUIBootcamp/SwiftUIBootcamp/Screens/GradientsScreen.swift
@@ -0,0 +1,27 @@
+//
+//  GradientsScreen.swift
+//  SwiftUIBootcamp
+//
+//  Created by Z003MK6T on 30/03/25.
+//
+
+import SwiftUI
+
+struct GradientsScreen: View {
+    var body: some View {
+        VStack(spacing: 50) {
+            RoundedRectangle(cornerRadius: 20)
+                .fill(LinearGradient(colors: [.red, .green], startPoint: .leading, endPoint: .trailing))
+                .frame(width: 300, height: 100)
+            
+            Circle()
+                .fill(RadialGradient(colors: [.blue, .yellow], center: .center, startRadius: 0, endRadius: 100))
+                .frame(width: 300, height: 100)
+            
+        }
+    }
+}
+
+#Preview {
+    GradientsScreen()
+}
