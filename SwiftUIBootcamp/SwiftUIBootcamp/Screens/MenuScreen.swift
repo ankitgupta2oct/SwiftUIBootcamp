02e92992c562617bc9e653bdfc5ea933d4a72917
Ankit <ankitgupta2oct.com>
2025-05-05 20:31:23 +0530

    Menu screen

---
+++ SwiftUIBootcamp/SwiftUIBootcamp/Screens/MenuScreen.swift
@@ -0,0 +1,38 @@
+//
+//  MenuScreen.swift
+//  SwiftUIBootcamp
+//
+//  Created by z0042k3y on 05/05/25.
+//
+
+import SwiftUI
+
+struct MenuScreen: View {
+    var body: some View {
+        Menu("Select Item") {
+            Button("Item 1") {
+                
+            }
+            
+            Button("Item 2") {
+                
+            }
+            
+            Button("Item 3") {
+                
+            }
+            
+            Button("Item 4") {
+                
+            }
+            
+            Button("Item 5") {
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    MenuScreen()
+}
